f6cdacbb39fd7280fc700735b79d9e92704ae165
Vraj Bhatt <vraj@Vrajs-MacBook-Pro.local>
2024-06-19 17:27:29 -0400

    views_auth created for authentication views and views.py contains other functionality views

--- CompanyApp/urls.py
+++ CompanyApp/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import CompanyRegisterView, CompanyLoginView, HomeView, CompanyLogoutView, GetCSRFToken
+from .views_auth import *
+from .views import *
 
 urlpatterns = [
     path('register/', CompanyRegisterView.as_view(), name='company-register'),
     path('login/', CompanyLoginView.as_view(), name='company-login'),
     path('logout/', CompanyLogoutView.as_view(), name='company-logout'),
-    path('home/', HomeView.as_view(), name='home'),
     path('get-csrf-token/', GetCSRFToken.as_view(), name='get-csrf-token'),
+    path('home/', home, name='home'),
 ]
