7a5ff8309c5a195e778f4fae8737b98f04525cc5
Riya Shah <shah6r1@uwindsor.ca>
2024-07-14 15:08:33 -0400

    Delete API Changes Done

--- RecruiterApp/views.py
+++ RecruiterApp/views.py
@@ -283,3 +283,30 @@ def get_candidateData(request):
     except Exception as e:
         print(e)
         return Response({'error': 'Internal Server Error', 'status': status.HTTP_500_INTERNAL_SERVER_ERROR})
+
+@api_view(['DELETE'])
+def delete_candidateData(request):
+    try:
+        response, isAuthenticated = get_company(request)
+
+        if not isAuthenticated:
+            return Response(response)
+        
+        company = response
+        if request.method == 'DELETE':
+            candidateID_list = request.data.get('id')
+
+            if not isinstance(candidateID_list, list):
+                candidateID_list = [candidateID_list]
+            
+            for candidate_id in candidateID_list:
+                candidate_id = decrypt(candidate_id)
+                candidate = CandidateData.objects.get(id=candidate_id)
+                if not candidate:
+                    return Response({'error': 'Candidate not found', 'status': status.HTTP_404_NOT_FOUND})
+                
+                candidate.delete()
+
+            return Response({'message': 'Selected candidates deleted successfully', 'status': status.HTTP_200_OK})
+    except Exception as e:
+        return Response({'error': 'Internal Server Error', 'status': status.HTTP_500_INTERNAL_SERVER_ERROR})
