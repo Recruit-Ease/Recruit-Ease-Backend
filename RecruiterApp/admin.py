11482aca751c099a9ecee65ac22cbfcf36f3d4dc
Riya Shah <shah6r1@uwindsor.ca>
2024-07-14 15:02:32 -0400

    candidates api changes done with major changes in getting data posting wise

--- RecruiterApp/admin.py
+++ RecruiterApp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Company, Posting, CandidateData
 
 # Register your models here.
+admin.site.register(Company)
+admin.site.register(Posting)
+admin.site.register(CandidateData)
--- RecruiterApp/views.py
+++ RecruiterApp/views.py
@@ -237,3 +237,49 @@ def save_candidateData(request):
             return Response({'message': 'Candidate data saved successfully', 'status': status.HTTP_201_CREATED})
     except Exception as e:
         return Response({'error': "Internal Server Error", 'status': status.HTTP_500_INTERNAL_SERVER_ERROR})
+
+# View to get the candidate data
+@api_view(['GET'])
+def get_candidateData(request):
+    try:
+        response, isAuthenticated = get_company(request)
+
+        if not isAuthenticated:
+            return Response(response)
+        
+        company = response
+        
+        id = request.GET.get('id')
+        posting_id = request.GET.get('posting_id')
+        if id:
+            candidateData = CandidateData.objects.filter(id=decrypt(id))
+        elif posting_id:
+            candidateData = CandidateData.objects.filter(posting_id=decrypt(posting_id))
+        else:
+            candidateData = CandidateData.objects.filter(posting__company=company)
+
+        data = []
+        for candidate in candidateData:
+            data.append({
+                'id': encrypt(candidate.id),
+                'posting_id': encrypt(candidate.posting.id),
+                'first_name': candidate.first_name,
+                'last_name': candidate.last_name,
+                'email': candidate.email,
+                'phone': candidate.phone,
+                'address': candidate.address,
+                'city': candidate.city,
+                'province': candidate.province,
+                'country': candidate.country,
+                'postal_code': candidate.postal_code,
+                'resume': candidate.resume.url,
+                'questions': candidate.questions,
+                'created_at': candidate.created_at,
+                'status': candidate.status
+            })
+        
+        return Response({'data': data, 'message': 'candidate data received successfully', 'status': status.HTTP_200_OK})
+
+    except Exception as e:
+        print(e)
+        return Response({'error': 'Internal Server Error', 'status': status.HTTP_500_INTERNAL_SERVER_ERROR})
