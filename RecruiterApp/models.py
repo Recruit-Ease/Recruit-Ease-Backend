dbda9d55511a1797541dc5acb46955836ad3004e
Riya Shah <shah6r1@uwindsor.ca>
2024-07-14 15:09:27 -0400

    Company Model added the str method

--- RecruiterApp/models.py
+++ RecruiterApp/models.py
@@ -7,6 +7,9 @@ class Company(models.Model):
     password = models.CharField(max_length=255)
     refresh_token = models.CharField(max_length=255, blank=True, null=True)
 
+    def __str__(self):
+        return self.name
+
     def set_password(self, raw_password):
         from django.contrib.auth.hashers import make_password
         self.password = make_password(raw_password)
@@ -69,4 +72,4 @@ class CandidateData(models.Model):
     status = models.CharField(max_length=50, choices=status_choices, default='Application Submitted')
 
     def __str__(self):
-        return self.first_name + " " + self.last_name
+        return self.first_name + " " + self.last_names
