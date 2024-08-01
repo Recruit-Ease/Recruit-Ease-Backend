c3ffb53fcbc2f35238ece72cc2a69e96732ed262
Riya Shah <shah6r1@uwindsor.ca>
2024-08-01 01:39:20 -0400

    Model Added Candidate Profile

--- CandidateApp/models.py
+++ CandidateApp/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.contrib.auth.hashers import make_password
+from django.contrib.auth.hashers import check_password
+import uuid
 
 class Candidate(models.Model):
     name = models.CharField(max_length=255)
@@ -10,14 +13,23 @@ class Candidate(models.Model):
         return self.name
 
     def set_password(self, raw_password):
-        from django.contrib.auth.hashers import make_password
         self.password = make_password(raw_password)
 
     def check_password(self, raw_password):
-        from django.contrib.auth.hashers import check_password
         return check_password(raw_password, self.password)
 
     def generate_refresh_token(self):
-        import uuid
         self.refresh_token = str(uuid.uuid4())
         self.save()
+
+class CandidateProfile(models.Model):
+    candidate = models.OneToOneField(Candidate, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    phone = models.CharField(max_length=15)
+    country = models.CharField(max_length=255)
+    address = models.TextField()
+    short_bio = models.TextField()
+
+    def __str__(self):
+        return self.candidate.name
